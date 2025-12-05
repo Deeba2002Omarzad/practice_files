e8571079e9348770b6eeedda51810e7fdeed0676
Deeba <deebaomarzad11@gmail.com>
2025-12-05 11:00:36 +0500

    Add three Python function files

---
+++ add_numbers.py
@@ -0,0 +1,11 @@
+def add_numbers(a,b):
+    sum = a + b
+    return sum
+
+
+num1 = 10
+num2 = 20
+
+c = add_numbers(num1,num2)
+
+print (c)
---
+++ multiply.py
@@ -0,0 +1,10 @@
+def multiply(a,b):
+    mul = a * b
+    return mul
+
+
+num1 = 10
+num2 = 20
+
+c= multiply(num1,num2)
+print(c)
